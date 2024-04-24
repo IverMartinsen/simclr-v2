b0cacb82a9713e8b08c42c883796e12dd7966f21
Iver Martinsen <iver.martinsen@outlook.com>
2024-04-24 19:31:39 +0200

    minor change

--- tf2/run.py
+++ tf2/run.py
@@ -470,8 +470,9 @@ def main(argv):
 
   #builder = tfds.builder(FLAGS.dataset, data_dir=FLAGS.data_dir)
   #builder.download_and_prepare()
-  builder = tfds.builder_from_directory('/Users/ima029/Desktop/SCAMPI/Repository/data/zip scrapings (huge)/data1/')
-
+  #builder = tfds.builder_from_directory('/Users/ima029/Desktop/SCAMPI/Repository/data/zip scrapings (huge)/data1/')
+  builder = tfds.builder_from_directory(FLAGS.data_dir)
+  
   num_train_examples = builder.info.splits[FLAGS.train_split].num_examples
   #num_eval_examples = builder.info.splits[FLAGS.eval_split].num_examples
   num_classes = builder.info.features['label'].num_classes
